4d4d59a148f26985de265df710c31c3b709ce2f4
zeetwy <zeetwy0@gmail.com>
2024-05-10 16:57:42 +0200

    Added SQL scripts to create tables for convenience.

---
+++ SQL_scripts/ServiceCategory.sql
@@ -0,0 +1,20 @@
+USE [DBSafouaneAntoine]
+GO
+
+/****** Object:  Table [dbo].[ServiceCategory]    Script Date: 10/05/2024 07:02:39 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[ServiceCategory](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[name] [varchar](50) NOT NULL,
+ CONSTRAINT [PK_ServiceCategory] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
